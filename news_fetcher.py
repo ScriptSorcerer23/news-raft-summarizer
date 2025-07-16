d1c276c2a3c914c556ae7217720072a1cad4c032
Chaudhary Sumama <6sumamatahir@gmail.com>
2025-07-16 14:09:49 +0500

    Update news_fetcher.py

--- news_fetcher.py
+++ news_fetcher.py
@@ -1,9 +1,11 @@
-
-# news_fetcher.py
-
+import os
 import requests
+from dotenv import load_dotenv
 
-gnews_api_key = "95c5a1b86451aa76c69a98593bda0721"
+load_dotenv()  
+
+gnews_api_key = os.getenv("GNEWS_API_KEY")
+
 
 def fetch_news(query: str, max_results: int = 5):
     url = f"https://gnews.io/api/v4/search?q={query}&lang=en&max={max_results}&token={gnews_api_key}"
