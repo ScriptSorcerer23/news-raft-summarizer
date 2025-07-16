2fc5fc657983b782918f65d651372ed396f64fc5
Chaudhary Sumama <6sumamatahir@gmail.com>
2025-07-16 12:52:53 +0500

    Update summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -1,14 +1,14 @@
+from transformers import pipeline, AutoModelForSeq2SeqLM, AutoTokenizer
 
-# summarizer.py
 
-from transformers import pipeline
+tokenizer = AutoTokenizer.from_pretrained("Sumama13/raft-summarizer")
+model = AutoModelForSeq2SeqLM.from_pretrained("Sumama13/raft-summarizer")
 
-# Load summarization pipeline
-summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
+
+summarizer = pipeline("summarization", model=model, tokenizer=tokenizer)
 
 def summarize_text(text: str, max_length: int = 130, min_length: int = 30):
     if not text or len(text.strip()) < 30:
         return "Text too short to summarize."
-
     summary = summarizer(text, max_length=max_length, min_length=min_length, do_sample=False)
     return summary[0]['summary_text']
